629923ad10b2cc8ddaaf2ea9214344fa67fba5fd
Justin Gammell <Liemander@outlook.com>
2025-04-18 12:59:08 -0500

    4/18

--- obj/Debug/net8.0/PainforGlory_Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/PainforGlory_Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707a1d56d1d90b06d36f6ff5a7f7ce61be255864")]
 [assembly: System.Reflection.AssemblyProductAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
