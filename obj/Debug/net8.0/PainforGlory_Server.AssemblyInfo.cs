a79bf53aa6a083b8b108b1750ef30153aa310c43
Justin Gammell <Liemander@outlook.com>
2025-04-28 07:59:04 -0500

    Adding admin area 4/28

--- obj/Debug/net8.0/PainforGlory_Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/PainforGlory_Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694eae517688ae652d4815c8380835da947f28fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c197c9e704795cca736574c4a41ca68cf64b615d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PainforGlory_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
